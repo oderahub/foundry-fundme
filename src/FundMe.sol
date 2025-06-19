0c35d82590aab67ecfc9d48e5bfe73e5ecd0eadf
oderahub <okere_chidera@yahoo.com>
2025-06-19 19:18:00 +0100

    chore: added more coverage test

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -2,79 +2,87 @@
 
 pragma solidity >=0.8.18;
 
-import {PriceConverter} from "./PriceConverter.sol";
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from './PriceConverter.sol';
+import {AggregatorV3Interface} from '@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol';
 
 error FundMe__NotOwner();
 
 contract FundMe {
-    using PriceConverter for uint256;
+  using PriceConverter for uint256;
 
-    uint256 public constant MINIMUM_USD = 5e18;
+  uint256 public constant MINIMUM_USD = 5e18;
 
-    address public immutable i_owner;
+  address public immutable i_owner;
 
-    AggregatorV3Interface private s_priceFeed;
+  AggregatorV3Interface private s_priceFeed;
 
-    constructor(address priceFeed) {
-        i_owner = msg.sender;
-        s_priceFeed = AggregatorV3Interface(priceFeed);
+  constructor(address priceFeed) {
+    i_owner = msg.sender;
+    s_priceFeed = AggregatorV3Interface(priceFeed);
+  }
+
+  address[] private s_funders;
+
+  mapping(address funder => uint256 amountFunded) private s_addressToAmountFunded;
+
+  function fund() public payable {
+    require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, 'did not send enough eth');
+
+    // msg.value.getConversionRate();
+    s_funders.push(msg.sender);
+    s_addressToAmountFunded[msg.sender] = s_addressToAmountFunded[msg.sender] += msg.value;
+  }
+
+  function withdraw() public onlyOwner {
+    for (uint256 funderIndex = 0; funderIndex < s_funders.length; funderIndex++) {
+      address funder = s_funders[funderIndex];
+      s_addressToAmountFunded[funder] = 0;
     }
 
-    address[] public funders;
+    s_funders = new address[](0);
 
-    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+    //transfer
+    //send
+    // call
 
-    function fund() public payable {
-        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "did not send enough eth");
+    // payable(msg.sender).transfer(address(this).balance);
 
-        // msg.value.getConversionRate();
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
-    }
+    // //send
 
-    function withdraw() public onlyOwner {
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
-        }
+    // bool sendSucess = payable(msg.sender).send(address(this).balance);
+    // require(sendSucess, "send failed.");
 
-        funders = new address[](0);
+    //call
 
-        //transfer
-        //send
-        // call
+    (bool callSucess, ) = payable(msg.sender).call{value: address(this).balance}('');
+    require(callSucess, 'Call failed.');
+  }
 
-        // payable(msg.sender).transfer(address(this).balance);
+  modifier onlyOwner() {
+    // require(msg.sender == owner, "only owner can call this function.");
+    // _;
 
-        // //send
+    if (msg.sender != i_owner) revert FundMe__NotOwner();
+    _;
+  }
 
-        // bool sendSucess = payable(msg.sender).send(address(this).balance);
-        // require(sendSucess, "send failed.");
+  function getVersion() public view returns (uint256) {
+    return s_priceFeed.version();
+  }
 
-        //call
+  receive() external payable {
+    fund();
+  }
 
-        (bool callSucess,) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSucess, "Call failed.");
-    }
+  fallback() external payable {
+    fund();
+  }
 
-    modifier onlyOwner() {
-        // require(msg.sender == owner, "only owner can call this function.");
-        // _;
+  function getAddressToAmoutFunded(address fundingAddress) external view returns (uint256) {
+    return s_addressToAmountFunded[fundingAddress];
+  }
 
-        if (msg.sender != i_owner) revert FundMe__NotOwner();
-        _;
-    }
-
-    function getVersion() public view returns (uint256) {
-        return s_priceFeed.version();
-    }
-
-    receive() external payable {
-        fund();
-    }
-
-    fallback() external payable {
-        fund();
-    }
+  function getFunder(uint256 index) external view returns (address) {
+    return s_funders[index];
+  }
 }
--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -2,38 +2,57 @@
 
 pragma solidity >=0.8.20;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console} from 'forge-std/Test.sol';
 
-import {FundMe} from "../src/FundMe.sol";
+import {FundMe} from '../src/FundMe.sol';
 
-import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {DeployFundMe} from '../script/DeployFundMe.s.sol';
 
 contract FundMeTest is Test {
-    FundMe fundMe;
+  FundMe fundMe;
 
-    function setUp() external {
-        // fundMe = new FundMe(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);
-        DeployFundMe deployFundMe = new DeployFundMe();
-        fundMe = deployFundMe.run();
-    }
+  address USER = makeAddr('user');
 
-    function testMinimumusdisfive() public view {
-        assertEq(fundMe.MINIMUM_USD(), 5e18);
-    }
+  uint256 constant SEND_VALUE = 0.1 ether;
 
-    function testOwnerisMsgSender() public view {
-        // console.log("msg.sender", msg.sender);
-        // console.log("owner", fundMe.owner());
-        assertEq(fundMe.i_owner(), msg.sender);
-    }
+  uint256 STARTING_BALANCE = 10 ether;
 
-    function testPriceFeedVersionIsAccurate() public {
-        if (block.chainid == 11155111) {
-            uint256 version = fundMe.getVersion();
-            assertEq(version, 4);
-        } else if (block.chainid == 1) {
-            uint256 version = fundMe.getVersion();
-            assertEq(version, 6);
-        }
+  function setUp() external {
+    // fundMe = new FundMe(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);
+    DeployFundMe deployFundMe = new DeployFundMe();
+    fundMe = deployFundMe.run();
+    vm.deal(USER, STARTING_BALANCE);
+  }
+
+  function testMinimumusdisfive() public view {
+    assertEq(fundMe.MINIMUM_USD(), 5e18);
+  }
+
+  function testOwnerisMsgSender() public view {
+    // console.log("msg.sender", msg.sender);
+    // console.log("owner", fundMe.owner());
+    assertEq(fundMe.i_owner(), msg.sender);
+  }
+
+  function testPriceFeedVersionIsAccurate() public view {
+    if (block.chainid == 11155111) {
+      uint256 version = fundMe.getVersion();
+      assertEq(version, 4);
+    } else if (block.chainid == 1) {
+      uint256 version = fundMe.getVersion();
+      assertEq(version, 6);
     }
+  }
+
+  function testFundMeFailWithoutEnoughEth() public {
+    vm.expectRevert();
+    fundMe.fund{value: 4e10}();
+  }
+
+  function testFundMeUpdatesDataStructureWhenFunded() public {
+    vm.prank(USER);
+    fundMe.fund{value: SEND_VALUE}();
+    uint256 amountFunded = fundMe.getAddressToAmoutFunded(USER);
+    assertEq(amountFunded, SEND_VALUE);
+  }
 }
